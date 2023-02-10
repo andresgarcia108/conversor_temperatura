d0b54f20875798755784a351fdacdaf0f3969f58
andresgarcia108 <andresgarcia@colegioguanenta.edu.co>
2023-02-10 09:11:28 -0500

    Se agregó el programa en python

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -1,6 +1,20 @@
 # Programa coversor de temperatura
-print ("------------------------------------")
-print ("------VALOR EN GRADOS CENTIGRADOS---")
-print ("------------------------------------")
+print ("---------------------------------------")
+print ("------VALOR EN GRADOS CENTIGRADOS------")
+print ("---------------------------------------")
 
 #input
+
+c = int(input("Digite el valor de grados centígrados:"))
+
+#processing
+f =(c+273.15)
+k =(c*1.8+32)
+
+#output
+print("----------------------------------")
+print("------------RESULTADOS------------")
+print("----------------------------------")
+
+print("Resultado grados farenheit " + str(f))
+print("Resultados grados kelvin " + str(k))
